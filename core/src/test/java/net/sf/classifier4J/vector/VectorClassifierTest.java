edd8f24961e1cac324ffccc4396ec8f99f96db94
Sébastien Gruchet <gruchet@gmail.com>
2016-01-15 21:37:15 +0100

    Conversion to JUnit4

--- core/src/test/java/net/sf/classifier4J/vector/VectorClassifierTest.java
+++ core/src/test/java/net/sf/classifier4J/vector/VectorClassifierTest.java
@@ -2,81 +2,50 @@
 package net.sf.classifier4J.vector;
 
 import net.sf.classifier4J.ClassifierException;
-import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
 
-public class VectorClassifierTest extends TestCase {
+public class VectorClassifierTest {
 
-    String sentence1 = "hello there is this a long sentence yes it is blah. blah hello";
-    
-    /*
-     * Class under test for double classify(String, String)
-     */
-    public void testClassifyStringString() {
+    private String sentence1 = "hello there is this a long sentence yes it is blah. blah hello";
+
+    @Test
+    public void testClassifyStringString() throws ClassifierException {
         TermVectorStorage storage = new HashMapTermVectorStorage();
         VectorClassifier vc = new VectorClassifier(storage);
-        try {
-            String category = "test";
-            vc.teachMatch(category, sentence1);
-            assertEquals(0.852d, vc.classify(category, "hello blah"), 0.001);
-            assertEquals(0.301d, vc.classify(category, "sentence"), 0.001);
-            assertEquals(0.0d, vc.classify(category, "bye"), 0.001);
-            
-            assertEquals(0.0d, vc.classify("does not exist", "bye"), 0.001);
-        } catch (ClassifierException e) {
-            e.printStackTrace();
-            fail(e.getLocalizedMessage());
-        }            
+
+        String category = "test";
+        vc.teachMatch(category, sentence1);
+        assertEquals(0.852d, vc.classify(category, "hello blah"), 0.001);
+        assertEquals(0.301d, vc.classify(category, "sentence"), 0.001);
+        assertEquals(0.0d, vc.classify(category, "bye"), 0.001);
+        assertEquals(0.0d, vc.classify("does not exist", "bye"), 0.001);
     }
 
-    /*
-     * Class under test for boolean isMatch(String, String)
-     */
-    public void testIsMatchStringString() {
+    @Test
+    public void testIsMatchStringString() throws ClassifierException {
         TermVectorStorage storage = new HashMapTermVectorStorage();
         VectorClassifier vc = new VectorClassifier(storage);
-        try {
-            String category = "test";
-            vc.teachMatch(category, sentence1);
-            assertTrue(vc.isMatch(category, "hello blah"));
-            assertFalse(vc.isMatch(category, "sentence"));
-            assertFalse(vc.isMatch(category, "bye"));
-        } catch (ClassifierException e) {
-            e.printStackTrace();
-            fail(e.getLocalizedMessage());
-        }            
+        String category = "test";
+        vc.teachMatch(category, sentence1);
+        assertTrue(vc.isMatch(category, "hello blah"));
+        assertFalse(vc.isMatch(category, "sentence"));
+        assertFalse(vc.isMatch(category, "bye"));
     }
 
-    /*
-     * Class under test for void teachMatch(String, String)
-     */
-    public void testTeachMatchStringString() {
+    @Test
+    public void testTeachMatchStringString() throws ClassifierException {
         TermVectorStorage storage = new HashMapTermVectorStorage();
         VectorClassifier vc = new VectorClassifier(storage);
-        try {
-            String category = "test";
-            vc.teachMatch(category, sentence1);
-            TermVector tv = storage.getTermVector(category);
-            assertNotNull("TermVector should not be null", tv);
-            assertEquals(5, tv.getTerms().length);
-            assertEquals(tv.getValues().length, tv.getTerms().length);
-            assertEquals("blah", tv.getTerms()[0]);
-            assertEquals(2, tv.getValues()[0]);
-            assertEquals("hello", tv.getTerms()[1]);
-            assertEquals(2, tv.getValues()[1]);
-            assertEquals("long", tv.getTerms()[2]);
-            assertEquals(1, tv.getValues()[2]);
-            assertEquals("sentence", tv.getTerms()[3]);
-            assertEquals(1, tv.getValues()[3]);
-            assertEquals("yes", tv.getTerms()[4]);
-            assertEquals(1, tv.getValues()[4]);
-                        
-            assertEquals("{[blah, 2] [hello, 2] [long, 1] [sentence, 1] [yes, 1] }", tv.toString());
-        } catch (ClassifierException e) {
-            e.printStackTrace();
-            fail(e.getLocalizedMessage());
-        }
-    }
-    
+        String category = "test";
+        vc.teachMatch(category, sentence1);
+        TermVector tv = storage.getTermVector(category);
+        assertThat(tv.getTerms()).containsExactly("blah", "hello", "long", "sentence", "yes");
+        assertThat(tv.getValues()).containsExactly(2, 2, 1, 1, 1);
 
+        assertEquals("{[blah, 2] [hello, 2] [long, 1] [sentence, 1] [yes, 1] }", tv.toString());
+    }
 }
