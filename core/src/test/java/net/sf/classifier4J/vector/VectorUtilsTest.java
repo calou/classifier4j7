af6cbb36ba79ee6b8aea608578ad0fe15e93c6df
Sébastien Gruchet <gruchet@gmail.com>
2016-01-15 21:43:41 +0100

    Conversion to JUnit4

--- core/src/test/java/net/sf/classifier4J/vector/VectorUtilsTest.java
+++ core/src/test/java/net/sf/classifier4J/vector/VectorUtilsTest.java
@@ -1,77 +1,65 @@
 package net.sf.classifier4J.vector;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class VectorUtilsTest extends TestCase {
+import static org.junit.Assert.assertEquals;
 
+public class VectorUtilsTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testScalarProduct1() {
+        VectorUtils.scalarProduct(new int[]{1, 2, 3}, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testScalarProduct2() {
+        VectorUtils.scalarProduct(null, new int[]{1, 2, 3});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testScalarProduct3() {
+        VectorUtils.scalarProduct(new int[]{1}, new int[]{1, 2, 3});
+    }
+
+    @Test
     public void testScalarProduct() {
-        try {
-            double result = VectorUtils.scalarProduct(new int[] { 1, 2, 3 }, null);
-            fail("Null argument allowed");
-        } catch (IllegalArgumentException e) {
-            // expected
-        }
-        
-        try {
-            double result = VectorUtils.scalarProduct(null, new int[] { 1, 2, 3 });
-            fail("Null argument allowed");
-        } catch (IllegalArgumentException e) {
-            // expected
-        }
-
-        try {
-            double result = VectorUtils.scalarProduct(new int[] {1}, new int[] { 1, 2, 3 });
-            fail("Arguments of different size allowed");
-        } catch (IllegalArgumentException e) {
-            // expected
-        }
-        
-        assertEquals(3, VectorUtils.scalarProduct(new int[] {1,1,1}, new int[] { 1,1,1}));
-        assertEquals(6, VectorUtils.scalarProduct(new int[] {1,1,1}, new int[] { 1,2,3}));
-        assertEquals(14, VectorUtils.scalarProduct(new int[] {1,2,3}, new int[] { 1,2,3 }));
-        assertEquals(0, VectorUtils.scalarProduct(new int[] {0,0,0}, new int[] { 1,2,3 }));
-
+        assertEquals(3, VectorUtils.scalarProduct(new int[]{1, 1, 1}, new int[]{1, 1, 1}));
+        assertEquals(6, VectorUtils.scalarProduct(new int[]{1, 1, 1}, new int[]{1, 2, 3}));
+        assertEquals(14, VectorUtils.scalarProduct(new int[]{1, 2, 3}, new int[]{1, 2, 3}));
+        assertEquals(0, VectorUtils.scalarProduct(new int[]{0, 0, 0}, new int[]{1, 2, 3}));
     }
 
+    @Test(expected = IllegalArgumentException.class)
     public void testVectorLength() {
-        try {
-            double result = VectorUtils.vectorLength(null);
-            fail("Null argument allowed");
-        } catch (IllegalArgumentException e) {
-            // expected
-        }        
-    
-        assertEquals(Math.sqrt(2), VectorUtils.vectorLength(new int[]{1,1}),0.001d);
-        assertEquals(Math.sqrt(3), VectorUtils.vectorLength(new int[]{1,1,1}),0.001d);
-        assertEquals(Math.sqrt(12), VectorUtils.vectorLength(new int[]{2,2,2}),0.001d);
+        VectorUtils.vectorLength(null);
     }
 
+    @Test
+    public void testScalarProduct4() {
+        assertEquals(Math.sqrt(2), VectorUtils.vectorLength(new int[]{1, 1}), 0.001d);
+        assertEquals(Math.sqrt(3), VectorUtils.vectorLength(new int[]{1, 1, 1}), 0.001d);
+        assertEquals(Math.sqrt(12), VectorUtils.vectorLength(new int[]{2, 2, 2}), 0.001d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void testCosineOfVectors() {
-        try {
-            double result = VectorUtils.cosineOfVectors(new int[] { 1, 2, 3 }, null);
-            fail("Null argument allowed");
-        } catch (IllegalArgumentException e) {
-            // expected
-        }
-        
-        try {
-            double result = VectorUtils.cosineOfVectors(null, new int[] { 1, 2, 3 });
-            fail("Null argument allowed");
-        } catch (IllegalArgumentException e) {
-            // expected
-        }
-        
-        try {
-            double result = VectorUtils.cosineOfVectors(new int[] {1}, new int[] { 1, 2, 3 });
-            fail("Arguments of different size allowed");
-        } catch (IllegalArgumentException e) {
-            // expected
-        }        
-        
-        int[] one = new int[]{1,1,1};
-        int[] two = new int[]{1,1,1};
-        
+        VectorUtils.cosineOfVectors(new int[]{1, 2, 3}, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCosineOfVectors2() {
+        VectorUtils.cosineOfVectors(null, new int[]{1, 2, 3});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCosineOfVectors3() {
+        VectorUtils.cosineOfVectors(new int[]{1}, new int[]{1, 2, 3});
+    }
+
+    @Test
+    public void testCosineOfVectors4() {
+        int[] one = new int[]{1, 1, 1};
+        int[] two = new int[]{1, 1, 1};
         assertEquals(1d, VectorUtils.cosineOfVectors(one, two), 0.001);
-    }    
-    
+    }
 }
